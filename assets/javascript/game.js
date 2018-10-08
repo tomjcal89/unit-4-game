5e4ef9b1447140e3a75bc3f6631fc4cf215ee9a7
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-07 22:33:48 -0700

    have a random number in between 19 and 120 generate for the winning number. also have each of the "jewels" generating a random number inbetween 1 and 12

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,14 +13,15 @@ $(document).ready(function () {
     var jewel2 = "";
     var jewel3 = "";
     var jewel4 = "";
-
+    var winningNumberID = 19 + Math.floor(Math.random() * 101);
+    
     // making functions for the Winning number and the jewels to calcualte random numbers
-    function randomNumberGeneratorWinning () {
+    function randomNumberGeneratorWinning() {
         winningNumber = "";
     }
 
     function randomNumberGeneratorJewels() {
-        
+
         jewel1 = "";
         jewel2 = "";
         jewel3 = "";
@@ -28,16 +29,27 @@ $(document).ready(function () {
     }
 
     $("#totalScoreId").text(totalScore);
-    $("#winsId").text("Wins: " + 0)
-    $("#lossesId").text("Losses: " + 0)
+   
+    
 
     //    when the webpage loads, need to make a random number populate as the "Winning Number". this number generate inbetween the numbers 19 and 120
-    
+    $("#winningNumber").text(winningNumberID);
 
 
     //when page loads, wins/losses and "total score" will all be set to 0
+    $("#winsId").text("Wins: " + 0);
+    $("#lossesId").text("Losses: " + 0);
 
     // each jewel will need to store a different random number in it. the random jewel number will generate inbetween 1 and 12
+    jewel1 = 1 + Math.floor(Math.random() * 12);
+    jewel2 = 1 + Math.floor(Math.random() * 12);
+    jewel3 = 1 + Math.floor(Math.random() * 12);
+    jewel4 = 1 + Math.floor(Math.random() * 12);
+    console.log(jewel1)
+    console.log(jewel2)
+    console.log(jewel3)
+    console.log(jewel4)
+
 
 
     //when someone clicks on the random jewel, the number will be populated in the total score div. (if there is already a number in the "total score", then the new number will be added to the "total score".)
