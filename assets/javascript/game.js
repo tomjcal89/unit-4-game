15edd62bb39fe21423bf7903316431d1e215e313
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-07 20:33:15 -0700

    added a little css to see the images and added initial score for total score to show on page

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,49 @@
- //  ---------java script---------
+//  ---------java script---------
+//When page loades
 
- //   need to establish variables
+$(document).ready(function () {
 
- var wins = 0;
- var losses = 0;
- var winningNumber = "";
- var totalScore = 0;
- var jewel1 = "";
- var jewel2 = "";
- var jewel3 = "";
- var jewel4 = "";
+    //   need to establish variables
 
-
- // making functions for the Winning number and the jewels to calcualte random numbers
-
- function randomNumberGenerator (){
-     winningNumber = "";
-     jewel1 = "";
-     jewel2 = "";
-     jewel3 = "";
-     jewel4 = "";
- }
-
- //    when the webpage loads, need to make a random number populate as the "Winning Number". this number generate inbetween the numbers 19 and 120
-
- 
-
- //when page loads, wins/losses and "total score" will all be set to 0
-
- // each jewel will need to store a different random number in it. the random jewel number will generate inbetween 1 and 12
+    var wins = 0;
+    var losses = 0;
+    var winningNumber = "";
+    var totalScore = 0;
+    var jewel1 = "";
+    var jewel2 = "";
+    var jewel3 = "";
+    var jewel4 = "";
 
 
- //when someone clicks on the random jewel, the number will be populated in the total score div. (if there is already a number in the "total score", then the new number will be added to the "total score".)
+    // making functions for the Winning number and the jewels to calcualte random numbers
+
+    function randomNumberGenerator() {
+        winningNumber = "";
+        jewel1 = "";
+        jewel2 = "";
+        jewel3 = "";
+        jewel4 = "";
+    }
+
+    $("#totalScoreID").text(totalScore);
 
 
-// if the "total score" goes beyong the "Winning Number", then the player loses and losses will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
-
-//if the "total score" matches the "winning number",  then the player wins and win's will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
+    //    when the webpage loads, need to make a random number populate as the "Winning Number". this number generate inbetween the numbers 19 and 120
 
 
+
+    //when page loads, wins/losses and "total score" will all be set to 0
+
+    // each jewel will need to store a different random number in it. the random jewel number will generate inbetween 1 and 12
+
+
+    //when someone clicks on the random jewel, the number will be populated in the total score div. (if there is already a number in the "total score", then the new number will be added to the "total score".)
+
+
+    // if the "total score" goes beyong the "Winning Number", then the player loses and losses will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
+
+    //if the "total score" matches the "winning number",  then the player wins and win's will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
+
+
+
+});
