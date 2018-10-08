863ddca59387e94e8d8b33e82f9547d027e3ce92
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-07 23:18:36 -0700

    added some changes to my game.js page to make it a little cleaner. had some duplicate items that were making it confusing to look at and what i had to do next.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,34 +7,18 @@ $(document).ready(function () {
 
     var wins = 0;
     var losses = 0;
-    var winningNumber = "";
     var totalScore = 0;
     var jewel1 = "";
     var jewel2 = "";
     var jewel3 = "";
     var jewel4 = "";
-    var winningNumberID = 19 + Math.floor(Math.random() * 101);
-    
-    // making functions for the Winning number and the jewels to calcualte random numbers
-    function randomNumberGeneratorWinning() {
-        winningNumber = "";
-    }
+    var winningNumber = 19 + Math.floor(Math.random() * 101);
 
-    function randomNumberGeneratorJewels() {
-
-        jewel1 = "";
-        jewel2 = "";
-        jewel3 = "";
-        jewel4 = "";
-    }
-
+    // putting initial total score on screen
     $("#totalScoreId").text(totalScore);
-   
-    
 
     //    when the webpage loads, need to make a random number populate as the "Winning Number". this number generate inbetween the numbers 19 and 120
-    $("#winningNumber").text(winningNumberID);
-
+    $("#winningNumberId").text(winningNumber);
 
     //when page loads, wins/losses and "total score" will all be set to 0
     $("#winsId").text("Wins: " + 0);
@@ -45,17 +29,18 @@ $(document).ready(function () {
     jewel2 = 1 + Math.floor(Math.random() * 12);
     jewel3 = 1 + Math.floor(Math.random() * 12);
     jewel4 = 1 + Math.floor(Math.random() * 12);
-    console.log(jewel1)
-    console.log(jewel2)
-    console.log(jewel3)
-    console.log(jewel4)
+   
 
-
+    // ------------------------------------------------------------------//
+    //do i need the random numbers to not generate the same number? do i need to have them generate even and odd numbers? 
+    //can someone always win the game?
+    // ------------------------------------------------------------------//
 
     //when someone clicks on the random jewel, the number will be populated in the total score div. (if there is already a number in the "total score", then the new number will be added to the "total score".)
-
+    $
 
     // if the "total score" goes beyong the "Winning Number", then the player loses and losses will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
+
 
     //if the "total score" matches the "winning number",  then the player wins and win's will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
 
