199232c15afee30417e82342f882e5dc539529f6
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-07 18:50:30 -0700

    added pseudocode to make sure i dont miss any steps when building my game

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+ //  ---------java script---------
+
+ //    when the webpage loads, need to make a random number populate as the "Winning Number". this number generate inbetween the numbers 19 and 120
+ 
+
+ //when page loads, wins/losses and "total score" will all be set to 0
+
+ // each jewel will need to store a different random number in it. the random jewel number will generate inbetween 1 and 12
+
+
+ //when someone clicks on the random jewel, the number will be populated in the total score div. (if there is already a number in the "total score", then the new number will be added to the "total score".)
+
+
+// if the "total score" goes beyong the "Winning Number", then the player loses and losses will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
+
+//if the "total score" matches the "winning number",  then the player wins and win's will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
+
+
