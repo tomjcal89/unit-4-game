4c88ec4851ca28fbdd50bc3349da8dd3473b184b
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-08 23:31:31 -0700

    organizing code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -17,23 +17,21 @@ $(document).ready(function () {
 
     // putting initial total score on screen
     $("#totalScoreId").text(totalScore);
-    
+
 
     //    when the webpage loads, need to make a random number populate as the "Winning Number". this number generate inbetween the numbers 19 and 120
     $("#winningNumberId").text(winningNumber);
 
     //when page loads, wins/losses and "total score" will all be set to 0
 
-    function winUpdate () {
-    $("#winsId").text("Wins: " + wins);
-    
+    function winUpdate() {
+        $("#winsId").text("Wins: " + wins);
     }
-
-    function lossUpdate (){
+    function lossUpdate() {
         $("#lossesId").text("Losses: " + losses);
     }
 
-    
+
 
 
     // each jewel will need to store a different random number in it. the random jewel number will generate inbetween 1 and 12
@@ -44,60 +42,60 @@ $(document).ready(function () {
 
     //when someone clicks on the random jewel, the number will be populated in the total score div. (if there is already a number in the "total score", then the new number will be added to the "total score".)
 
-    $("#jewel1Id").on("click", function() {
+    $("#jewel1Id").on("click", function () {
 
-        if(jewel1 + totalScore) {
-        totalScore = jewel1 + totalScore;
-        $("#totalScoreId").text(totalScore);
+        if (jewel1 + totalScore) {
+            totalScore = jewel1 + totalScore;
+            $("#totalScoreId").text(totalScore);
         }
     });
 
-    $("#jewel2Id").on("click", function() {
+    $("#jewel2Id").on("click", function () {
 
-        if(jewel2 + totalScore) {
-        totalScore = jewel2 + totalScore;
-        $("#totalScoreId").text(totalScore);
+        if (jewel2 + totalScore) {
+            totalScore = jewel2 + totalScore;
+            $("#totalScoreId").text(totalScore);
         }
     });
 
-    $("#jewel3Id").on("click", function() {
+    $("#jewel3Id").on("click", function () {
 
-        if(jewel3 + totalScore) {
-        totalScore = jewel3 + totalScore;
-        $("#totalScoreId").text(totalScore);
+        if (jewel3 + totalScore) {
+            totalScore = jewel3 + totalScore;
+            $("#totalScoreId").text(totalScore);
         }
     });
 
-    $("#jewel4Id").on("click", function() {
+    $("#jewel4Id").on("click", function () {
 
-        if(jewel4 + totalScore) {
-        totalScore = jewel4 + totalScore;
-        $("#totalScoreId").text(totalScore);
+        if (jewel4 + totalScore) {
+            totalScore = jewel4 + totalScore;
+            $("#totalScoreId").text(totalScore);
         }
     });
 
-    
+
     // calling out the functions for winning and lossing
     winUpdate()
     lossUpdate()
-    
+
 
     // if the "total score" goes beyong the "Winning Number", then the player loses and losses will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
 
-    
+
     if (winningNumber === totalScore) {
-        wins ++;
+        wins++;
         winUpdate()
-        
-    } 
-    
-    if (winningNumber < totalScore) {
-        losses ++;
-        lossUpdate()
-        
+
     }
 
-    
+    if (winningNumber < totalScore) {
+        losses++;
+        lossUpdate()
+
+    }
+
+
     //if the "total score" matches the "winning number",  then the player wins and win's will go up by 1, "winning number" will generate a new random number inbetween 19-120, the "total score" will be set back to 0, and the jewels will all reset to new random numbers inbetween 1-12.
 
 
@@ -107,6 +105,5 @@ $(document).ready(function () {
 
 
 
-  
+
     //do i need the random numbers to not generate the same number? do i need to have them generate even and odd numbers? can someone always win the game?//
-   
